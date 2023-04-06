1e54999e9017aec751f7167f30d2b9ce8b6ac70a
Savio Nascimento <savio.nascimento.gomes@gmail.com>
2023-04-05 22:09:36 -0300

    reorganizacao da estrutura de pasta do projeto

---
+++ GerenciadorSupermercadoFront/src/app/model/supermercado.model.ts
@@ -0,0 +1,8 @@
+export class SupermecadoModel {
+  constructor(
+      public id: number,
+      public nome: string,
+      public fone: string,
+      public endereco: string
+  ) {}
+}
--- src/app/model/supermercado.model.ts
+++
@@ -1,8 +0,0 @@
-export class SupermecadoModel {
-  constructor(
-      public id: number,
-      public nome: string,
-      public fone: string,
-      public endereco: string
-  ) {}
-}
